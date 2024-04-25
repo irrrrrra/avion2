e9d3d9ab9c4444f47971da04c5a9ffb9cbed56b8
irrrrrra <146156144+irrrrrra@users.noreply.github.com>
2024-04-25 20:49:04 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
 const burger = () => {
-    const btn = document.querySelector('.burger svg:last-child')
-    
-    const off = document.querySelector('.offset')
-    const close = document.querySelector('.offset nav svg')
-    btn.addEventListener('click', () => {
-        off.style.display = 'block'
-        console.log(btn)
-    })
-    close.addEventListener('click', () => {
-        off.style.display = 'none'
-    })
+   const btn = document.querySelector('.burger svg:last-child')
+   const off = document.querySelector('.offset')
+   const close = document.querySelector('.offset nav svg')
+   btn.addEventListener('click', () => {
+      off.style.display = 'block'
+   })
+
+   close.addEventListener('click', () => {
+      off.style.display = 'none'
+   })
 }
 
 burger()
